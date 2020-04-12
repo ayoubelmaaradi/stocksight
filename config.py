7e6aa5b55d724a361b971d1ff36d4ea68e7b96c9
ayoubelmaaradi <ayoub1913@outlook.com>
2020-04-12 03:06:07 +0200

    c1

---
+++ config.py
@@ -0,0 +1,21 @@
+elasticsearch_host = "localhost"
+elasticsearch_port = 9200
+elasticsearch_user = ""
+elasticsearch_password = ""
+consumer_key= 'p9smvhLm9SXh9z2EUNYyakFe7'
+consumer_secret= 'Fu6f5YWPyEfrUwpGS02Ojm2eMSxanySYzvkhP7ypVLtlCvIvgy'
+access_token= '809287937351249920-2S2imOpJVEy0oxDlGwVADh3wM2WPVpY'
+access_token_secret= 'EkF2JQx9jcsBOyyMU4UsZYgFqLWJ1UH0W8NCDPL7Axnbs'
+nltk_tokens_required = ("neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink")
+nltk_min_tokens = 1
+nltk_tokens_ignored = ("win", "giveaway")
+twitter_feeds = ["@elonmusk", "@cnbc", "@benzinga", "@stockwits",
+                 "@Newsweek", "@WashingtonPost", "@breakoutstocks", "@bespokeinvest",
+                 "@WSJMarkets", "@stephanie_link", "@nytimesbusiness", "@IBDinvestors",
+                 "@WSJDealJournal", "@jimcramer", "@TheStalwart", "@TruthGundlach",
+                 "@Carl_C_Icahn", "@ReformedBroker", "@bespokeinvest", "@stlouisfed",
+                 "@muddywatersre", "@mcuban", "@AswathDamodaran", "@elerianm",
+                 "@MorganStanley", "@ianbremmer", "@GoldmanSachs", "@Wu_Tang_Finance",
+                 "@Schuldensuehner", "@NorthmanTrader", "@Frances_Coppola", "@bySamRo",
+                 "@BuzzFeed","@nytimes"]
+stocksight_token = "88a04fdd499fa296ad18af85cdab1eaa3fad06c123f50cf6ec4c404f06e387434a327641c5cdfda61e7acf7d96ec40d261aa"
